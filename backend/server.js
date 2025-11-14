8ab805f291285f8d426e3fc21235d4d93982ad57
Prasanth210 <prasanthps8838@gmail.com>
2025-11-14 20:33:04 +0530

    bug fix

--- backend/server.js
+++ backend/server.js
@@ -23,6 +23,6 @@ app.post("/shortest-path", (req, res) => {
   }
 });
 
-app.listen(5000, () => {
-  console.log("Backend running on http://localhost:5000");
-});
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Backend running on ${PORT}`));
+
